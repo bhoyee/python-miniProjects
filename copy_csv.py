75c64ecf4072b33f5d790e703bdde7044325b6d5
unknown <s.adeboye@outlook.com>
2023-03-21 17:00:38 +0000

    update the script copy multiple file to db

--- copy_csv.py
+++ copy_csv.py
@@ -15,3 +15,23 @@ def copy_csv_to_postgresql(csv_folder_path, db_name, db_user, db_password, db_ho
         port=db_port
     )
     cur = conn.cursor()
+
+    
+    # Loop through each CSV file in the folder
+    for filename in os.listdir(csv_folder_path):
+        if filename.endswith('.csv'):
+            # Open the CSV file and read the contents
+            with open(os.path.join(csv_folder_path, filename), 'r') as f:
+                # Create a temporary table to hold the CSV data
+                cur.execute('CREATE TEMP TABLE tmp_table (LIKE {} INCLUDING ALL)'.format(sql.Identifier(filename[:-4])))
+                # Load the CSV data into the temporary table
+                cur.copy_expert('COPY tmp_table FROM STDIN WITH CSV HEADER', f)
+                # Insert the data from the temporary table into the destination table
+                cur.execute('INSERT INTO {} SELECT * FROM tmp_table'.format(sql.Identifier(filename[:-4])))
+                # Drop the temporary table
+                cur.execute('DROP TABLE tmp_table')
+
+    # Commit the changes and close the connection
+    conn.commit()
+    cur.close()
+    conn.close()
