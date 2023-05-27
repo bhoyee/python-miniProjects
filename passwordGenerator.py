15236779723194d4371bf3845c69cbf34b5b7100
unknown <s.adeboye@outlook.com>
2023-05-27 13:37:24 +0100

    add passwordGenerator.py

---
+++ passwordGenerator.py
@@ -0,0 +1,8 @@
+import random
+
+print('Welcome To Your Password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%*76(),.#'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
