2c39b7638554e865debb5792139cc44e484d25f6
unknown <s.adeboye@outlook.com>
2023-05-27 13:43:09 +0100

    updated passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -6,3 +6,14 @@ chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%*76(),.#'
 
 number = input('Amount of passwords to generate: ')
 number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nHere are the passwords: ')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
